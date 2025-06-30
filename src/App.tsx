c0328485b3da27993c8613f2a2383ccf876eaf6e
Igor Brandalise <brandaliseigor123@gmail.com>
2025-06-30 16:11:07 -0300

    import form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './index.css'
+import { Formulario } from './components/formulario'
 
 function App() {
 
 
   return (
-  <div>
-
-  </div>
+ <main className='flex border-2 border-gray-400 w-[40rem]  justify-center m-auto p-6'>
+  <Formulario/>
+ </main>
   )
 }
 
