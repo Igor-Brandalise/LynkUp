6a089de045686df7bc8cf9a763c9a9d277ada4b1
Igor Brandalise <brandaliseigor123@gmail.com>
2025-07-01 22:38:55 -0300

    alinhando o main

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './index.css'
-import { Formulario } from './components/formulario'
+import "./index.css";
+import { Formulario } from "./components/formulario";
 
 function App() {
-
-
   return (
- <main className='flex border-2 border-gray-400 w-[40rem]  justify-center m-auto p-6'>
-  <Formulario/>
- </main>
-  )
+    <body className="flex justify-center items-center mt-[5rem]">
+      <main className="flex border-2 border-gray-400 w-[40rem]  justify-center items-center m-auto p-6">
+        <Formulario />
+      </main>
+    </body>
+  );
 }
 
-export default App
+export default App;
