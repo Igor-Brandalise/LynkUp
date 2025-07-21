a24ee50dd4f2d7139fd222dbbbc5c5959da4579f
Igor Brandalise <brandaliseigor123@gmail.com>
2025-07-20 23:35:14 -0300

    layout e checagem

--- src/components/formulario.tsx
+++ src/components/formulario.tsx
@@ -1,25 +1,59 @@
-export function Formulario() {
+import { useState } from "react";
+import type { Favorito } from "../types/favorito";
+
+type Props = {
+  aoAdicionar: (favorito: Favorito) => void;
+};
+
+export function Formulario({ aoAdicionar }: Props) {
+  const [titulo, setTitulo] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!titulo || !url) {
+      return;
+    }
+
+    const novoFavorito: Favorito = {
+      id: crypto.randomUUID(),
+      titulo,
+      url,
+    };
+
+    aoAdicionar(novoFavorito);
+
+    setTitulo("");
+    setUrl("");
+  };
+
   return (
-    <section className="flex flex-col items-center gap-5">
+    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-5">
       <h1 className="text-3xl">Gerenciador de bookmarks</h1>
+
       <input
         type="text"
+        value={titulo}
+        onChange={(e) => setTitulo(e.target.value)}
         placeholder="Digite seu título"
         className="w-[25rem] h-10 border-1 border-gray-300 rounded-[10px] p-2"
       />
+
       <input
         type="url"
-        name=""
-        id=""
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
         placeholder="Digite sua URL"
         className="w-[25rem] h-10 border-1 border-gray-300 rounded-[10px] p-2"
       />
-      <button className="text-[18px] w-[15rem] h-10 border-2 bg-blue-500 text-white border-blue-700 rounded-4xl hover:scale-105 hover:shadow-lg
-  transition-all duration-300 ease-in-out hover:bg-blue-700">
+
+      <button
+        type="submit"
+        className="text-[18px] w-[15rem] h-10 border-2 bg-blue-500 text-white border-blue-700 rounded-4xl hover:scale-105 hover:shadow-lg
+  transition-all duration-300 ease-in-out hover:bg-blue-700"
+      >
         Adicionar bookmark
       </button>
-    </section>
+    </form>
   );
 }
-
-<h1>Gerenciador de bookmarks</h1>;
