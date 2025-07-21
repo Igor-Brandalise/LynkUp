6e17836e3f93cf052f36d02dcd16be2f8f1d4600
Igor Brandalise <brandaliseigor123@gmail.com>
2025-07-20 23:33:58 -0300

    adicionar favorito e remover favorito

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import "./index.css";
 import { Formulario } from "./components/formulario";
+import { Favoritos } from "./components/favoritos";
+import type { Favorito } from "./types/favorito";
+import { useState } from "react";
 
 function App() {
+
+    const [favoritos, setFavoritos] = useState<Favorito[]>([]);
+
+  const adicionarFavorito = (novo: Favorito) => {
+    setFavoritos((prev) => [...prev, novo]);
+  };
+
+  const removerFavorito = (id: string) => {
+    setFavoritos((prev) => prev.filter((fav) => fav.id !== id));
+  };
+
   return (
-    <body className="flex justify-center items-center mt-[5rem]">
-      <main className="flex border-2 border-gray-400 w-[40rem]  justify-center items-center m-auto p-6">
-        <Formulario />
+    <div className="flex justify-center items-center mt-[5rem]">
+      <main className="flex border-2 border-gray-400 w-[40rem] rounded-2xl justify-center items-center m-auto p-6">
+
+        <Formulario aoAdicionar={adicionarFavorito}/>
+        <Favoritos lista={favoritos} aoRemover={removerFavorito} />
       </main>
-    </body>
+    </div>
   );
 }
 
