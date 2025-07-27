e043405285f4b67649b6cd0954f3f8e312f14f5c
Igor Brandalise <brandaliseigor123@gmail.com>
2025-07-27 14:58:53 -0300

    lista/itens adicionados

--- src/components/lista.tsx
+++ src/components/lista.tsx
@@ -1,7 +1,28 @@
-export function Lista (){
-    return(
-        <div>
-            
-        </div>
-    )
+import type { Favorito } from "../types/favorito";
+
+type props = {
+  itens: Favorito[];
+  aoRemover: (id: string) => void;
+};
+
+export function Lista({ itens, aoRemover }: props) {
+  return (
+    <ul className="flex flex-col gap-4 mt-6">
+      {itens.map((item) => (
+        <li key={item.id} className="flex justify-between items-center rounded-[10px] border p-2">
+          <a
+            href={item.url}
+            target="_blank"
+            rel="nooperner noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            {item.titulo}
+          </a>
+          <button onClick={() => aoRemover(item.id)} className="bg-red-900 text-white rounded-2xl px-3 py-1 hover:bg-red-600">
+            Remover
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
